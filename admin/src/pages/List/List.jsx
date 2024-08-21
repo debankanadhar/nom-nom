eb295c3adc7303071197a8699398cefbf0fa9b39
Arunkumar <110915475+iamarunhere@users.noreply.github.com>
2024-08-22 00:15:35 +0530

    removed a  console

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -6,7 +6,6 @@ const List = ({ url }) => {
   const [list, setList] = useState([]);
   const fetchList = async () => {
     const response = await axios.get(url + "/api/food/list");
-    console.log(response.data);
     if (response.data.success) {
       setList(response.data.data);
     } else {
