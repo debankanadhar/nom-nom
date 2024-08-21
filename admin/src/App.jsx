e411b7933e8b3537b24c39ec464389ffda8f71ee
Arunkumar <110915475+iamarunhere@users.noreply.github.com>
2024-08-21 23:51:32 +0530

    Updated backend url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Orders from "./pages/Orders/Orders";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const App = () => {
-  const url = "https://food-delivery-backend-0w83.onrender.com";
+  const url = "https://food-delivery-website-backend-b6qm.onrender.com";
   return (
     <div>
       <ToastContainer />
